90d3869d8964303b1bd4c68bbed7024c598b89c5
MuAlphaOmegaEpsilon <tommasobonvicini@gmail.com>
2020-01-13 00:14:21 +0100

    Create statistic.hpp and include it in algorythm.hpp

--- include/algorythm.hpp
+++ include/algorythm.hpp
@@ -1,5 +1,7 @@
 #ifndef ALGORYTHM_HPP
 #define ALGORYTHM_HPP
 
+#include <algorythm/statistic.hpp>
+
 #endif // ALGORYTHM_HPP
 
---
+++ include/algorythm/statistic.hpp
@@ -0,0 +1,4 @@
+#if !defined ALGORYTHM_STATISTIC_HPP
+	#define ALGORYTHM_STATISTIC_HPP
+	#include <algorythm/statistic/extremes.hpp>
+#endif // ALGORYTHM_STATISTIC_HPP
